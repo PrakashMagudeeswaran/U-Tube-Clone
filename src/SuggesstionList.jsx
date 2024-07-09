1300a3664fe5ae47daacee166b1ea0413a79c604
Prakash <157131108+PrakashMagudeeswaran@users.noreply.github.com>
2024-07-09 13:05:37 +0530

    Update SuggesstionList.jsx

--- src/SuggesstionList.jsx
+++ src/SuggesstionList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { setkeyword } from '../Components/homeSlice'
 import { useDispatch } from 'react-redux'
-const Suggestions=["All","Music","Cinema","Gaming","Rahman","HarrisJayaraj","FullStack","React","Potate","Naruto","Movies","Anime","Jafar Panahi","C++","Data Structures","Fries","Christian Bale"]
+const Suggestions=["All","Music","MongoDB","Gaming","Rahman","HarrisJayaraj","FullStack","React","Potate","Naruto","Movies","Node JS","Jafar Panahi","C++","Data Structures","Fries","Christian Bale"]
 const SuggesstionList = () => {
   const dispatch=useDispatch()
   return (
     <div className="suggestwhole">
     <div className="suggest">
-     {Suggestions.map(suggestion=><p
+     {Suggestions.map(suggestion,index)=><p key={index}
      onClick={()=>{dispatch(setkeyword(suggestion))}}>{suggestion}</p>)} 
       
 
